7be46ebb42e2cd6ccd076f9517737113aa78f9c4
Nguyenle <Nguyenle.httdn@gmail.com>
2022-06-13 15:00:47 +0700

    1

--- src/BachHoaXanh.Web/Pages/Bills/CreateModal.js
+++ src/BachHoaXanh.Web/Pages/Bills/CreateModal.js
@@ -31,6 +31,15 @@
     );
 });
 var arrayCard = [];
+
+/*ẩn nút save*/
+$(document).ready(() => {
+    if (arrayCard.length == 0) {
+        document.getElementById("save").hidden = true;
+        document.getElementById("Sumh2").hidden = true;
+    }
+});
+
 /*thực hiện click vào class="btn-add"*/
 $(document).on('click', '.btn-add', function () {
     var id = this.id
@@ -44,17 +53,23 @@ $(document).on('click', '.btn-add', function () {
                         <td>${res.name}</td>
                         <td> ${res.unitPrice}</td>
                         <td>
-                              <i class="bi bi-dash-circle" id="${res.id}" onclick=" Quantity('dow',this.id)" style="width: 20%"></i>
+                              <i class="bi bi-dash-circle" id="${res.id}" onclick=" Quantity('dow',this.id, ${res.unitPrice})" style="width: 20%"></i>
                               <input id="quantity${res.id}" value=${quantity} style="width: 60%" />
-                              <i id="${res.id}" class="bi bi-plus-circle" onclick="Quantity('up',this.id)" style="width: 20%"></i>
+                              <i id="${res.id}" class="bi bi-plus-circle" onclick="Quantity('up',this.id,${res.unitPrice})" style="width: 20%"></i>
                         </td>
                         <td>
-                            <h3 id="Sum"> sum </h3>
+                            <span id="span${res.id}">
+                                ${res.unitPrice}
+                            </span>
                         </td>
                         <td>
                             <i class="bi bi-x-circle-fill" id="${res.id}" onclick="Delete(this.id)"></i>
                         </td>
                     </tr>`
+        if (quantity == 1) {
+            $(`#span${res.id}`).text("test");
+        }
+        
         /*kiểm tra id đã có trong arry chưa*/
         if (!arrayCard.includes(id)) {
             /*thêm giá trị id vào arry*/
@@ -63,33 +78,56 @@ $(document).on('click', '.btn-add', function () {
         } else {
             /*chuyển giá trị value thành int*/
             let value = parseInt($("#quantity" + id).val()) + 1;
+            /*xử lý trường hợp sản phẩm đã có thì cộng giá trị lên 1*/
             $("#quantity" + id).val(value);
         }
-    })        
+    })
+    document.getElementById("save").hidden = false;
+    document.getElementById("Sumh2").hidden = false;
 })
-function Quantity(type, id) {
+function Quantity(type, id, unitPrice) {
+
+    calc_total();
+
     /*lấy giá trị ô input tại id của input*/
     let value = $("#quantity" + id).val();
     if (type === 'up') {
         value++;
-
+        var sum = unitPrice * value;
+        $("#span" + id).text(sum);
     } else {
-
         value--;
+        var sum = unitPrice * value;
+        $("#span" + id).text(sum);
     }
     if (value < 0) {
         value = 0;
+        $("#span" + id).text("0");
         /*thông báo chuyển đến hàm xóa row*/
-        abp.message.confirm('Are you sure to delete the "admin" role?')
+        abp.message.confirm('Are you sure to delete')
             .then(function (confirmed) {
                 if (confirmed) {
                     Delete(id);
                 }
             });
+        calc_total();
     }
     /*gán giá trị cho ô input*/
     $("#quantity" + id).val(value)
 }
+
+function calc_total() {
+    var sum = 0;
+    $("[id^=span]").each(() => {
+        /*sum = sum + parseFloat($(this).text());
+        console.log(sum);*/
+        var id = this.id;
+        console.log(id);
+    });
+    /*$("#rsSum").text("Sum: " + sum);*/
+};
+
+
 
 /*xóa dòng*/
 function Delete(id) {
@@ -98,4 +136,9 @@ function Delete(id) {
     var index = arrayCard.indexOf(id);
     /*xóa phần tử tại vị trí và xóa 1 phần tử*/
     arrayCard.splice(index, 1);
+    if (arrayCard.length == 0) {
+        /*load lại trang*/
+        location.reload();
+        return;
+    }
 }
